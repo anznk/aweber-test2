6981089790a091563b2c4b4a7e579d3547cdcdc5
Anzu <anzunakayama@gmail.com>
2020-07-20 16:25:19 -0700

    20200720

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+// import img
+// import larry3 from "../materials/picture/larry3.jpg";
+import send from "../materials/Button/SendButton.svg";
+import fbIcon from "../materials/SNS_icon/Facebook_Logo_CircleWhite.svg";
+import twitterIcon from "../materials/SNS_icon/Twitter_Logo_CircleWhite.svg";
+// constants
+const fbUrl = "https://www.facebook.com/lintonwellness/";
+const twtterUrl = "https://twitter.com/lintonto";
+const textP = "Want to hear more from me?";
+const email = "larry@LiveWellWithPD.com"
+const phoneNum = "(416) 710 6546";
+
+
+const Footer = () => {
+  useEffect(() => {
+    let urlParamStr = window.location.search
+
+    if (urlParamStr) {
+      urlParamStr = urlParamStr.substring(1)
+      let params = {}
+
+      urlParamStr.split('&').forEach( param => {
+        const temp = param.split('=')
+        params = {
+          ...params,
+          [temp[0]]: temp[1]
+        }
+      })
+    }
+  });
+  return (
+    <>
+    <form action="https://www.aweber.com/scripts/addlead.pl" method="post"> 
+      <input type="hidden" name="listname" value="awlist5671012" />
+      <input type="hidden" name="redirect" value="https://anzunakayama.dev/?success=true" />
+      {/* <input type="hidden" name="meta_redirect_onlist" value="https://www.google.ca/" />  */}
+      <input type="hidden" name="meta_adtracking" value="custom form" />
+      <input type="hidden" name="meta_message" value="1" /> 
+      <input type="hidden" name="meta_required" value="email" /> 
+      <input type="hidden" name="meta_forward_vars" value="1" /> 
+      <input type="text" name="email"  /> Email
+      <input type="submit" name="submit" value="Subscribe" /> 
+    </form>
+    </>
+
+  );
+};
+
+export default Footer;

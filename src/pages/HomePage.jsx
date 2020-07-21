c68e1d5d66b9823f6ed8082de13954a19d3b4fdd
Anzu <anzunakayama@gmail.com>
2020-07-20 17:21:12 -0700

    add redirects

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ function HomePage() {
           <Route exact path="/" component={Home} />
           {/* <Route path="/about" component={About} />
           <Route path="/test" component={Test} /> */}
-          <Route exact path="/book" component={BookLanding} />
+          <Route path="/book" component={BookLanding} />
         </Switch>
       </Router>
     </>
